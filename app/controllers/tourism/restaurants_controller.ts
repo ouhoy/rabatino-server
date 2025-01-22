243ef6c890b4104517b9843816eeb78284b0be9f
Abdallah Dahmou <kimkinon@gmail.com>
2025-01-22 02:28:09 +0100

    Add restuarant crud

--- app/controllers/tourism/restaurants_controller.ts
+++ app/controllers/tourism/restaurants_controller.ts
@@ -1,6 +1,9 @@
 import type { HttpContext } from '@adonisjs/core/http'
 import Restaurant from '#models/restaurant'
-import { restaurantValidator } from '#validators/tourism'
+import { restaurantValidator, tourismValidator } from '#validators/tourism'
+import Post from '#models/post'
+import { postValidator } from '#validators/post'
+import TourismPost from '#models/tourism_post'
 
 export default class RestaurantsController {
   /**
@@ -18,9 +21,20 @@ export default class RestaurantsController {
    * Handle form submission for the create action
    */
   async store({ request, response }: HttpContext) {
-    const data = await request.validateUsing(restaurantValidator)
+    const postData = await request.validateUsing(postValidator)
+    const tourismContainer = await request.validateUsing(tourismValidator)
+    const tourismAttraction = await request.validateUsing(restaurantValidator)
 
-    const post = await Restaurant.create(data)
+    const createdPost = await Post.create(postData)
+    const tourismPost = await TourismPost.create({
+      ...tourismContainer,
+      postId: createdPost.id,
+    })
+
+    const post = await Restaurant.create({
+      ...tourismAttraction,
+      tourismPostId: tourismPost.id,
+    })
     response.ok(post)
   }
 
@@ -28,12 +42,23 @@ export default class RestaurantsController {
    * Handle form submission for the edit action
    */
   async update({ request, response, params }: HttpContext) {
-    const data = await request.validateUsing(restaurantValidator)
-    const postId = params.id
+    const postData = await request.validateUsing(postValidator)
+    const tourismContainer = await request.validateUsing(tourismValidator)
+    const tourismAttractionPost = await request.validateUsing(restaurantValidator)
 
-    const post = await Restaurant.findOrFail(postId)
-    post.merge(data)
-    await post.save()
+    // First find the college record
+    const college = await Restaurant.findOrFail(params.id)
+
+    // Find and update the educational institution
+    const educationalInstitution = await TourismPost.findOrFail(college.tourismPostId)
+
+    // Find and update the base post
+    const post = await Post.findOrFail(educationalInstitution.postId)
+
+    // Update all three models
+    await post.merge(postData).save()
+    await educationalInstitution.merge(tourismContainer).save()
+    await college.merge(tourismAttractionPost).save()
 
     response.ok(post)
   }
@@ -42,10 +67,18 @@ export default class RestaurantsController {
    * Show individual record
    */
   async show({ params, response }: HttpContext) {
-    const postId = params.id
-    const post = await Restaurant.findOrFail(postId)
-    await post.incrementViews()
-    return response.ok(post)
+    const attraction = await Restaurant.query()
+      .where('id', params.id)
+      .preload('touristAttraction', (query) => {
+        query.preload('post')
+      })
+      .firstOrFail()
+
+    // Increment views
+    await attraction.touristAttraction.post.incrementViews()
+
+    // Merge all data for complete response
+    return response.ok(attraction)
   }
 
   /**
@@ -53,7 +86,7 @@ export default class RestaurantsController {
    */
   async destroy({ params, response }: HttpContext) {
     const postId = params.id
-    const post = await Restaurant.findOrFail(postId)
+    const post = await Post.findOrFail(postId)
     await post.delete()
     return response.ok(true)
   }
