f9ca551a8b0dbfb36f7bc9dae4c9dd6526c420e5
Abdallah Dahmou <kimkinon@gmail.com>
2025-01-27 12:58:48 +0100

    Update ts file

--- app/controllers/auth/login_controller.ts
+++ app/controllers/auth/login_controller.ts
@@ -1,22 +1,20 @@
-import type {HttpContext} from '@adonisjs/core/http'
-import {loginValidator} from '#validators/auth'
+import type { HttpContext } from '@adonisjs/core/http'
+import { loginValidator } from '#validators/auth'
 import User from '#models/user'
 
 export default class LoginController {
-
-  async show({response, auth}: HttpContext) {
+  async show({ response, auth }: HttpContext) {
     // return auth.isAuthenticated ? 'You are already logged in' : 'Auth login Page is Ready!!'
-    return response.json({auth: auth.user})
+    return response.json({ auth: auth.user })
   }
 
-  async store({request, response, auth}: HttpContext) {
-    const data = await request.validateUsing(loginValidator);
+  async store({ request, response, auth }: HttpContext) {
+    const data = await request.validateUsing(loginValidator)
 
     const user = await User.verifyCredentials(data.email, data.password)
 
     await auth.use('web').login(user, data.isRememberMe)
 
-    return response.json({auth: auth.user})
+    return response.json({ auth: auth.user })
   }
-
 }
